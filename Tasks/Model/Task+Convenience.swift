34a22465d6b2fffcfc2e439b89a2de26d0e9b114
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2018-11-25 16:05:35 -0600

    Model created

---
+++ Tasks/Model/Task+Convenience.swift
@@ -0,0 +1,20 @@
+//
+//  Task+Convenience.swift
+//  Tasks
+//
+//  Created by Ilgar Ilyasov on 11/25/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Task {
+    convenience init(name: String, notes: String? = nil, managedObjectContex: NSManagedObjectContext) {
+        
+        self.init(context: managedObjectContex)
+        
+        self.name = name
+        self.notes = notes
+    }
+}
